4ff8200599af9e9cdfcc56e402a8e1332d63e29c
Christopher Toothaker <c.m.toothaker@gmail.com>
2022-05-29 22:50:46 -0400

    Completed Exercise

---
+++ UsingOperators/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace UsingOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 17;
+            var b = 4;
+            var div = a / b;
+            var mod = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+            }
+
+            else
+            {
+
+            }
+            Console.WriteLine($"What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var areaOfCircle = AreaOfCircle(radius);
+            Console.WriteLine($"The area of a circle with radius {radius} is {areaOfCircle}");
+
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * (radius * radius);
+        }
+    }
+    
+}
